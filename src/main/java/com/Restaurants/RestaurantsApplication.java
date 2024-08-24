87601feb5342206640dec1880d4e33cb5b6f6798
devNihar <patraniharranjan5@gmail.com>
2024-08-24 17:58:18 +0530

    code update

--- src/main/java/com/Restaurants/RestaurantsApplication.java
+++ src/main/java/com/Restaurants/RestaurantsApplication.java
@@ -2,8 +2,12 @@ package com.Restaurants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.Restaurants.Repository")
+@EntityScan(basePackages = "com.Restaurants.Model")
 public class RestaurantsApplication {
 
 	public static void main(String[] args) {
